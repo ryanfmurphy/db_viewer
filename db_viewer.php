702d1d15451339742f975c723dca19d5eba90f30
ryanfmurphy <ryanfmurphy@gmail.com>
2016-04-20 09:43:52 -0500

    separate openJoin() and closeJoin() into fns
    (cleanup)
    used to be embedded directly in thClickHandler

--- db_viewer.php
+++ db_viewer.php
@@ -207,247 +207,255 @@
 
 <script>
 
-        var CELL_CLASSES; // #todo delete
+    // fold / unfold via click
+    var tdClickHandler = function(e){
+        // alt to fold/unfold row
+        if (e.altKey) {
+            rowN = $(e.target).closest('tr').attr('data-row');
 
-        // fold / unfold via click
-		var tdClickHandler = function(e){
-            // alt to fold/unfold row
-            if (e.altKey) {
-                rowN = $(e.target).closest('tr').attr('data-row');
-
-                if (e.shiftKey) {
-                    unfoldRowsFrom(rowN);
-                }
-                else {
-                    hideRow(rowN);
-                }
-            }
-            // no alt to fold/unfold col
-            else {
-                colN = e.target.cellIndex;
-                if (e.shiftKey) {
-                    unfoldColsFrom(colN);
-                }
-                else {
-                    hideCol(colN);
-                }
-            }
-        };
-
-        function getColVals(cells) {
-            var vals = $.map(cells, function(n,i){return n.innerText});
-            return vals;
-        }
-
-        // util
-        function firstObjKey(obj) {
-            for (first in obj) break;
-            return first;
-        }
-        function getObjKeys(obj) {
-            var keys = [];
-            for (key in obj) keys.push(key);
-            return keys;
-        }
-        function getObjKeysRev(obj) {
-            var keys = [];
-            for (key in obj) keys.unshift(key);
-            return keys;
-        }
-
-        function showVal(val) {
-            if (val === null) {
-                return '';
+            if (e.shiftKey) {
+                unfoldRowsFrom(rowN);
             }
             else {
-                return val;
+                hideRow(rowN);
             }
         }
-
-        function isValidJoinField(field_name) {
-            return true;
-        }
-
-        // data is keyed by id
-        // add to HTML Table, lined up with relevant row
-        function addDataToTable(cells, data, exclude_fields) {
-            // exclude_fields is a hash with the fields to exclude as keys
-
-            var outerLevel = parseInt( cells.first().attr('level') )
-                                || 0;
-            var innerLevel = outerLevel + 1;
-
-            // get all fields
-            // by getting the keys of the first obj
-            var first_obj = data[firstObjKey(data)];
-
-            // we want field_names backwards so
-            // when we append them they are forwards
-            var field_names = getObjKeysRev(first_obj);
-
-            // loop thru cells and add additional cells after
-            cells.each(function(row_index,e){
-
-                var cols_open = 0;
-
-                // loop thru all fields and add a column for each row
-                for (i in field_names) {
-
-                    var field_name = field_names[i];
-                    if (field_name in exclude_fields) continue;
-
-                    var cell_val = e.innerHTML.trim();
-                    var key = cell_val;
-
-                    var val = (key in data
-                                    ? data[key][field_name]
-                                    : '');
-
-                    var TD_or_TH = e.tagName;
-                    var display_val = (TD_or_TH == 'TH'
-                                            ? field_name
-                                            : showVal(val));
-                    var content = '\
-                                <'+TD_or_TH+'                   \
-                                    class="level'+innerLevel+'" \
-                                    level="'+innerLevel+'"      \
-                                >                               \
-                                    '+display_val+'             \
-                                </'+TD_or_TH+'>                 \
-                               ';
-                    $(e).after(content);
-
-                    cols_open++;
-                }
-
-                $(e).addClass('level'+innerLevel+'handle')
-                    .attr('cols_open', cols_open);
-
-            });
-
-        }
-
-        function isTruthy(x) { return Boolean(x); }
-
-        function trimIfString(x) {
-            if (typeof x == 'string') {
-                return x.trim();
+        // no alt to fold/unfold col
+        else {
+            colN = e.target.cellIndex;
+            if (e.shiftKey) {
+                unfoldColsFrom(colN);
             }
             else {
-                return x;
+                hideCol(colN);
             }
         }
+    };
 
-        // click on header field name (e.g. site_id) - joins to that table, e.g. site
-        // displays join inline and allows you to toggle it back
-		var thClickHandler = function(e){
+    function getColVals(cells) {
+        var vals = $.map(cells, function(n,i){return n.innerText});
+        return vals;
+    }
 
-            var elem = e.target;
-            var col_no = elem.cellIndex;
+    // util
+    function firstObjKey(obj) {
+        for (first in obj) break;
+        return first;
+    }
+    function getObjKeys(obj) {
+        var keys = [];
+        for (key in obj) keys.push(key);
+        return keys;
+    }
+    function getObjKeysRev(obj) {
+        var keys = [];
+        for (key in obj) keys.unshift(key);
+        return keys;
+    }
 
+    function showVal(val) {
+        if (val === null) {
+            return '';
+        }
+        else {
+            return val;
+        }
+    }
+
+    function isValidJoinField(field_name) {
+        return true;
+    }
+
+    // data is keyed by id
+    // add to HTML Table, lined up with relevant row
+    function addDataToTable(cells, data, exclude_fields) {
+        // exclude_fields is a hash with the fields to exclude as keys
+
+        var outerLevel = parseInt( cells.first().attr('level') )
+                            || 0;
+        var innerLevel = outerLevel + 1;
+
+        // get all fields
+        // by getting the keys of the first obj
+        var first_obj = data[firstObjKey(data)];
+
+        // we want field_names backwards so
+        // when we append them they are forwards
+        var field_names = getObjKeysRev(first_obj);
+
+        // loop thru cells and add additional cells after
+        cells.each(function(row_index,e){
+
+            var cols_open = 0;
+
+            // loop thru all fields and add a column for each row
+            for (i in field_names) {
+
+                var field_name = field_names[i];
+                if (field_name in exclude_fields) continue;
+
+                var cell_val = e.innerHTML.trim();
+                var key = cell_val;
+
+                var val = (key in data
+                                ? data[key][field_name]
+                                : '');
+
+                var TD_or_TH = e.tagName;
+                var display_val = (TD_or_TH == 'TH'
+                                        ? field_name
+                                        : showVal(val));
+                var content = '\
+                            <'+TD_or_TH+'                   \
+                                class="level'+innerLevel+'" \
+                                level="'+innerLevel+'"      \
+                            >                               \
+                                '+display_val+'             \
+                            </'+TD_or_TH+'>                 \
+                           ';
+                $(e).after(content);
+
+                cols_open++;
+            }
+
+            $(e).addClass('level'+innerLevel+'handle')
+                .attr('cols_open', cols_open);
+
+        });
+
+    }
+
+    function isTruthy(x) { return Boolean(x); }
+
+    function trimIfString(x) {
+        if (typeof x == 'string') {
+            return x.trim();
+        }
+        else {
+            return x;
+        }
+    }
+
+    // elem is the pivot <th> element you click on to
+    // close all the rows that have been opened from the join
+    function closeJoin(elem) {
+
+        var col_no = elem.cellIndex;
+        var all_cells = nthCol(col_no);
+        var cols_open = parseInt($(elem).attr('cols_open'));
+
+        var handle_class;
+
+        { // find handle_class ("levelXhandle" class) to remove to undo color
+
+            // use first cell as an example - all of them are the same
+            var first_cell = all_cells.first();
+            // use native DomElement.classList
+            var cell_classes = first_cell.get(0).classList;
+
+            for (var i in cell_classes) {
+                var classname = cell_classes[i];
+                var is_handle_class = (classname.indexOf('handle') != -1);
+                if (is_handle_class) {
+                    handle_class = classname;
+                    break;
+                }
+            }
+
+        }
+
+        // for each row, remove all the open columns after that cell
+        all_cells.each(function(idx,elem){
+
+            // loop through and remove that many cells
+            for (var i = 0; i < cols_open; i++) {
+                $(elem).next().remove();
+            }
+
+            $(elem).removeClass(handle_class); // restores earlier color
+            $(elem).removeAttr('cols_open');
+
+        });
+    }
+
+    function openJoin(elem) {
+
+        var col_no = elem.cellIndex;
+        var field_name = elem.innerHTML.trim();
+
+        if (isValidJoinField(field_name)) {
+
+            var prefix = field_name.slice(0,-3);
+
+            // #todo validate prefix = valid table name?
+
+            { // request adjoining table data #todo split into function
+
+                { // figure out what data to ask for based on ids in col
+                    //var table_name = prefix;
+
+                    var all_cells = nthCol(col_no);
+                    var val_cells = all_cells.filter('td');
+                    var ids = getColVals(val_cells);
+                    var non_null_ids = ids.filter(isTruthy)
+                        .map(trimIfString)
+                    ;
+                    // #todo remove dups from non_null_ids
+
+                    var uri = 'query_id_in<?= $maybe_url_php_ext ?>';
+                    var request_data = {
+                        ids: non_null_ids,
+                        join_field: field_name
+                    };
+
+                }
+
+                { // make request
+                    $.ajax({
+                        url: uri,
+                        type: 'POST',
+                        data: request_data,
+                        dataType: 'json',
+                        success: function(data) {
+
+                            var exclude_fields = {};
+                            exclude_fields[field_name] = 1;
+
+                            addDataToTable(all_cells, data, exclude_fields);
+
+                        },
+                        error: function(r) {
+                            alert("Failure");
+                        }
+                    });
+                }
+            }
+        }
+        else {
+            alert("Cannot expand this field \""+field_name+"\" - it doesn't end in \"_id\"");
+        }
+    }
+
+    // click on header field name (e.g. site_id) - joins to that table, e.g. site
+    // displays join inline and allows you to toggle it back
+    var thClickHandler = function(e){
+        var elem = e.target;
+        if ($(elem).attr('cols_open')) {
             // already opened - close
-            if ($(elem).attr('cols_open')) {
-                var all_cells = nthCol(col_no);
-                var cols_open = parseInt($(elem).attr('cols_open'));
+            closeJoin(elem);
+        }
+        else {
+            // not already opened - do open
+            openJoin(elem);
+        }
+    };
 
-                var handle_class;
-
-                { // find handle_class ("levelXhandle" class) to remove to undo color
-
-                    // use first cell as an example - all of them are the same
-                    var first_cell = all_cells.first();
-                    // use native DomElement.classList
-                    var cell_classes = first_cell.get(0).classList;
-
-                    CELL_CLASSES = cell_classes; // #todo delete
-
-                    for (var i in cell_classes) {
-                        var classname = cell_classes[i];
-                        var is_handle_class = (classname.indexOf('handle') != -1);
-                        if (is_handle_class) {
-                            handle_class = classname;
-                            break;
-                        }
-                    }
-
-                }
-
-                // for each row, remove all the open columns after that cell
-                all_cells.each(function(idx,elem){
-
-                    // loop through and remove that many cells
-                    for (var i = 0; i < cols_open; i++) {
-                        $(elem).next().remove();
-                    }
-
-                    $(elem).removeClass(handle_class); // restores earlier color
-                    $(elem).removeAttr('cols_open');
-
-                });
-            }
-            else { // not already opened - do open
-                var field_name = elem.innerHTML.trim();
-
-                if (isValidJoinField(field_name)) {
-
-                    var prefix = field_name.slice(0,-3);
-
-                    // #todo validate prefix = valid table name?
-
-                    { // request adjoining table data #todo split into function
-
-                        { // figure out what data to ask for based on ids in col
-                            //var table_name = prefix;
-
-                            var all_cells = nthCol(col_no);
-                            var val_cells = all_cells.filter('td');
-                            var ids = getColVals(val_cells);
-                            var non_null_ids = ids.filter(isTruthy)
-                                .map(trimIfString)
-                            ;
-                            // #todo remove dups from non_null_ids
-
-                            var uri = 'query_id_in<?= $maybe_url_php_ext ?>';
-                            var request_data = {
-                                ids: non_null_ids,
-                                join_field: field_name
-                            };
-
-                        }
-
-                        { // make request
-                            $.ajax({
-                                url: uri,
-                                type: 'POST',
-                                data: request_data,
-                                dataType: 'json',
-                                success: function(data) {
-
-                                    var exclude_fields = {};
-                                    exclude_fields[field_name] = 1;
-
-                                    addDataToTable(all_cells, data, exclude_fields);
-
-                                },
-                                error: function(r) {
-                                    alert("Failure");
-                                }
-                            });
-                        }
-                    }
-                }
-                else {
-                    alert("Cannot expand this field \""+field_name+"\" - it doesn't end in \"_id\"");
-                }
-            }
-        };
-
-<?php if ($cmp) { ?>
-        $('td').live('click', tdClickHandler);
-        $('th').live('click', thClickHandler);
+<?php if ($oldJquery) { ?>
+    $('td').live('click', tdClickHandler);
+    $('th').live('click', thClickHandler);
 <?php } else { ?>
-        $('table').on('click', 'td', tdClickHandler);
-        $('table').on('click', 'th', thClickHandler);
+    $('table').on('click', 'td', tdClickHandler);
+    $('table').on('click', 'th', thClickHandler);
 <?php }
 ?>
 
