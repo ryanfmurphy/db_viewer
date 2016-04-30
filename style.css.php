195d00abe1934b617f3a968ba4a6f03f9f3cbe63
ryanfmurphy <ryanfmurphy@gmail.com>
2016-04-30 10:26:38 -0500

    css: $background option: light or dark

--- style.css.php
+++ style.css.php
@@ -1,4 +1,8 @@
 <?php
+	require_once('db_config.php');
+
+    if (!isset($background)) $background = 'light';
+
 	if (!isset($inlineCss) || !$inlineCss) {
 		header('Content-Type: text/css');
 	}
@@ -6,25 +10,57 @@
 /*<style>*/
 html {
     margin: 50px;
-    padding:0;
+    padding: 0;
     font-family: sans-serif;
 }
 body{
     margin: 0px;
-    padding:0;
+    padding: 0;
+<?php
+    if ($background=='dark') {
+?>
+    background: black;
+    color: white;
+<?php
+    }
+?>
 }
 table {
     border-spacing: 0;
+    border-collapse: collapse;
 }
 td, th {
     padding: .3em;
+<?php
+    if ($background=='dark') {
+?>
+    border: solid 1px #aaa; /* gray */
+<?php
+    }
+    else {
+?>
     border: solid 1px gray;
+<?php
+    }
+?>
 }
 
 textarea, input {
     font-family: sans-serif;
     margin: 1em;
 }
+
+textarea, input[type=text] {
+<?php
+    if ($background=='dark') {
+?>
+    background: black;
+    color: white;
+<?php
+    }
+?>
+}
+
 textarea {
     width: 90%;
     height: 5em;
