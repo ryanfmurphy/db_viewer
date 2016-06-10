b2efd2d12a364d29f1745407ac79d06562040209
ryanfmurphy <ryanfmurphy@gmail.com>
2016-06-09 19:43:10 -0500

    variablize table for dash form
    requestVars
    
    submit buttons for Create Update View and Delete
        variablize action - #todo remove unneeded var
    
    more fields2skip

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 {
     { # init - #todo don't rely on DbViewer
         include("../db_viewer/init.php");
+        $requestVars = array_merge($_GET, $_POST);
     }
 
     { #vars
-        $table = "example_table";
+        $table = isset($requestVars['table'])
+                    ? $requestVars['table']
+                    : "example_table";
 
         { # get fields
             $rows = Util::sql("
@@ -23,17 +26,28 @@
             #$fields = array("name","txt");
 
             { #todo fields2skip
-                if ($table == "example_table") {
-                    $fields2skip = array("example_field2omit");
-                }
-                else {
-                    $fields2skip = array("iid","id","time");
+                switch ($table) {
+                    case "example_table": {
+                        $fields2skip = array(
+                            "iid","id","time",
+                            "tags", #todo
+                        );
+                    } break;
+
+                    default:
+                        $fields2skip = array("iid","id","time");
                 }
             }
         }
+
+        $action = "create"; #todo or "update"
     }
 }
 ?>
+
+
+
+
 <html>
     <head>
         <title>Dash</title>
@@ -65,6 +79,12 @@ form#mainForm label {
     font-size: 80%;
 }
         </style>
+        <script>
+        function setFormAction(url) {
+            var form = document.getElementById('mainForm');
+            form.action = url;
+        }
+        </script>
     </head>
     <body>
         <p id="whoami">Dash</p>
@@ -72,7 +92,7 @@ form#mainForm label {
             <code><?= $table ?></code> table
         </h1>
 
-        <form id="mainForm" action="/ormrouter/create_<?= $table ?>">
+        <form id="mainForm" action="/ormrouter/<?= $action ?>_<?= $table ?>">
 <?php
     foreach ($fields as $name) {
         if (in_array($name, $fields2skip)) {
@@ -83,13 +103,21 @@ form#mainForm label {
                         : "input");
 ?>
             <div class="formInput">
-                <label for="<?= $name ?>"><?= $name ?></label>
+                <label for="<?= $name ?>">
+                    <?= $name ?>
+                </label>
                 <<?= $inputTag ?> name="<?= $name ?>"><?= "</$inputTag>" ?>
             </div>
 <?php
     }
 ?>
-            <input type="submit" />
+            <div id="submits">
+                <input onclick="setFormAction('/ormrouter/create_<?= $table ?>')" value="Create" type="submit" />
+                <input onclick="setFormAction('/ormrouter/update_<?= $table ?>')" value="Update" type="submit" />
+                <input onclick="setFormAction('/ormrouter/get_<?= $table ?>s?use_db_viewer=1')" value="View" type="submit" />
+                    <!-- set action="/ormrouter/get_<?= $table ?>s?use_db_viewer=1" -->
+                <input onclick="setFormAction('/ormrouter/delete_<?= $table ?>')" value="Delete" type="submit" />
+            </div>
         </form>
 
     </body>
