00223fb638299b16ee42ad60ca18f874272ab3b9
ryanfmurphy <ryanfmurphy@gmail.com>
2016-10-24 19:31:54 -0500

    Util class shim needed by dash

--- db_viewer/classes/db_stuff/DbUtil.php
+++ db_viewer/classes/db_stuff/DbUtil.php
@@ -1,4 +1,22 @@
 <?php
+if (!class_exists('Util')) {
+
+    class Util {
+
+        public static function endsWith($needle,$haystack) {
+            $len = strlen($needle);
+            $LEN = strlen($haystack);
+            return substr($haystack, $LEN - $len) == $needle;
+        }
+
+        public static function quote($val) {
+            global $db; #todo will this global work in all cases?
+            #todo #fixme might not work for nulls?
+            return $db->quote($val);
+        }
+    }
+
+}
 
 if (!class_exists('DbUtil')) {
 
