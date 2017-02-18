17a2e99f77012790190c2ba7e482950f28145c52
ryanfmurphy <ryanfmurphy@gmail.com>
2017-02-18 11:40:23 -0600

    helpful message if they didn't fill out db_config.php yet

--- setup.php
+++ setup.php
@@ -1,10 +1,23 @@
 <?php
 # work in progress
 ?>
+<!--
 <h1>
     Welcome to DB Viewer Setup
 </h1>
+-->
+<h1>
+    DB Viewer needs a <code>db_config.php</code> file
+</h1>
 
+<p>
+    You are here because you do not have a <code>db_config.php</code>
+    in your main DB Viewer folder.  You can copy the example config
+    <code>db_config.sample.php</code> and fill it in with your database
+    credentials.
+</p>
+
+<!--
 <p>
     There are plenty of config options - only a few are necessary to get started.
 </p>
@@ -23,4 +36,5 @@
         <input type="submit">
     </div>
 </form>
+-->
 
