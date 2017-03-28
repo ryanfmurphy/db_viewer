57c9129a091930f6946ddb4533ed836591372d6f
ryanfmurphy <ryanfmurphy@gmail.com>
2017-03-27 20:26:09 -0500

    more details in setup.php

--- setup.php
+++ setup.php
@@ -1,23 +1,59 @@
 <?php
 # work in progress
 ?>
-<!--
+<html>
+    <head>
+        <style>
+            body {
+                max-width: 700px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+        </style>
+    </head>
 <h1>
-    Welcome to DB Viewer Setup
-</h1>
--->
-<h1>
-    DB Viewer needs a <code>db_config.php</code> file
+    You're almost setup to View your DB!
 </h1>
 
+<h2>
+    DB Viewer needs a <code>db_config.php</code> file to run
+</h2>
+
 <p>
-    You are here because you do not have a <code>db_config.php</code>
-    in your main DB Viewer folder.  You can copy the example config
+    This screen is here because you tried to run DB Viewer
+    yet you do not have a <code>db_config.php</code>
+    file in your main DB Viewer folder.  You can copy the example config
     <code>db_config.sample.php</code> and fill it in with your database
     credentials.
 </p>
 
-<!--
+<p>
+    The <code>db_config.php</code> file allows you to activate
+    many optional features and configure many things about DB Viewer:
+</p>
+
+<ul>
+    <li>Database Credentials and Configuration</li>
+    <li>ID Naming Scheme</li>
+    <li>Field Choice</li>
+    <li>Background Color / Background Images</li>
+    <li>Custom Actions via Callback Function</li>
+    <li>URIs for various parts of DB Viewer to make it plug more easily into other systems</li>
+    <li>And much more</li>
+</ul>
+
+<p>
+    Have any questions about how to get started?
+    <a href="mailto:ryanfmurphy@gmail.com">
+        Ask Ryan!
+    </a>
+</p>
+
+<!-- #todo allow them to fill out basic options. save it in the file or in the DB?
+<h1>
+    Welcome to DB Viewer Setup
+</h1>
+
 <p>
     There are plenty of config options - only a few are necessary to get started.
 </p>
