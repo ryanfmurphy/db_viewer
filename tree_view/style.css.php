9db202ae48f3ab76e3b45157acda3c7630ade71e
ryanfmurphy <ryanfmurphy@gmail.com>
2017-05-08 09:17:17 -0500

    add style.css.php to repo

---
+++ tree_view/style.css.php
@@ -0,0 +1,57 @@
+<style>
+            body {
+                font-family: sans-serif;
+            }
+            h1 {
+                color: gray;
+                font-weight: normal;
+                text-align: center;
+            }
+            h1 span {
+                font-weight: bold;
+            }
+
+            .node {
+                cursor: pointer;
+            }
+
+            .node circle {
+                fill: #fff;
+                stroke: steelblue;
+                stroke-width: 1.5px;
+            }
+
+            .node text {
+                font: 10px sans-serif;
+            }
+
+            .link {
+                fill: none;
+                stroke: #ddd;
+                stroke-width: 1.5px;
+            }
+
+            #edit_vars_link {
+                text-decoration: none;
+            }
+            #edit_vars_link:hover {
+                text-decoration: underline;
+            }
+
+            #summary {
+                margin-left: .15em;
+            }
+
+<?php
+    foreach ($table_info as $table => $info) {
+        $color = $info['color'];
+?>
+            .<?= $table ?>_tbl_color {
+                color: <?= $color ?>;
+                stroke: <?= $color ?>;
+            }
+<?php
+    }
+?>
+
+</style>
