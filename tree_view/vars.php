9bcfb4fe4078f3cbf74d13cdeb69d1379bef97e7
ryanfmurphy <ryanfmurphy@gmail.com>
2017-06-26 02:13:21 -0500

    tree_view: restructure vars.php to track all vars in an array
    then use extract to still get the $vars into the scope
    I'm hoping this will simplify how other parts of Tree View
    deal with the vars by e.g. allowing them to loop thru the var names

--- tree_view/vars.php
+++ tree_view/vars.php
@@ -1,50 +1,77 @@
 <?php
-    $backend = isset($requestVars['backend'])
-                    ? $requestVars['backend']
-                    : 'db';
+    # An associative array with keys that are var names
+    # and values that are default values.
+    # These vars will be used throughout Tree View
+    # as the request variables.
+    $tree_var_names_w_default_values = array(
 
-    $root_table = isset($requestVars['root_table'])
-                    ? $requestVars['root_table']
-                    : null;
+        'backend' => isset($requestVars['backend'])
+                        ? $requestVars['backend']
+                        : 'db',
 
-    $root_cond = isset($requestVars['root_cond'])
-                 && $requestVars['root_cond'] !== ''
-                    ? $requestVars['root_cond']
-                    : "$default_parent_field is null";
+        'root_table' => isset($requestVars['root_table'])
+                            ? $requestVars['root_table']
+                            : null,
 
-    $order_by_limit = isset($requestVars['order_by_limit'])
-                        ? $requestVars['order_by_limit']
-                        : null;
+        'root_cond' => isset($requestVars['root_cond'])
+                       && $requestVars['root_cond'] !== ''
+                            ? $requestVars['root_cond']
+                            : "$default_parent_field is null",
 
-    $root_nodes_w_child_only = isset($requestVars['root_nodes_w_child_only'])
-                                    ? $requestVars['root_nodes_w_child_only']
-                                    : null;
+        'order_by_limit' => isset($requestVars['order_by_limit'])
+                                ? $requestVars['order_by_limit']
+                                : null,
 
-    $parent_relationships = isset($requestVars['parent_relationships'])
-                                ? $requestVars['parent_relationships']
-                                : ($assume_default_tree_relationship
-                                    ? array(
-                                            'child_table' => $root_table,
-                                            'parent_table' => $root_table,
-                                            'parent_field' => $default_parent_field,
-                                            'matching_field_on_parent' => '{{USE PRIMARY KEY}}',
-                                            # optional fields
-                                            'condition' => null,
-                                            'parent_filter_field' => null,
-                                            'parent_filter_field_val' => null,
-                                      )
-                                    : array()
-                                  );
+        'root_nodes_w_child_only' => isset($requestVars['root_nodes_w_child_only'])
+                                        ? $requestVars['root_nodes_w_child_only']
+                                        : null,
+        'parent_relationships' => isset($requestVars['parent_relationships'])
+                                    ? $requestVars['parent_relationships']
+                                    : ($assume_default_tree_relationship
+                                        ? array(
+                                                'child_table' => $root_table,
+                                                'parent_table' => $root_table,
+                                                'parent_field' => $default_parent_field,
+                                                'matching_field_on_parent' => '{{USE PRIMARY KEY}}',
+                                                # optional fields
+                                                'condition' => null,
+                                                'parent_filter_field' => null,
+                                                'parent_filter_field_val' => null,
+                                          )
+                                        : array()
+                                      ),
 
-    $name_cutoff = isset($requestVars['name_cutoff'])
-                        ? $requestVars['name_cutoff']
-                        : null;
+        'name_cutoff' => isset($requestVars['name_cutoff'])
+                            ? $requestVars['name_cutoff']
+                            : null,
 
-    $edit_vars = isset($requestVars['edit_vars'])
-                    ? $requestVars['edit_vars']
-                    : null;
+        'edit_vars' => isset($requestVars['edit_vars'])
+                            ? $requestVars['edit_vars']
+                            : null,
 
-    # make sure details are set for all relationships (default to null)
+        'use_stars_for_node_size' => (isset($requestVars['use_stars_for_node_size'])
+                                      && $requestVars['use_stars_for_node_size'] !== ''
+                                        ? $requestVars['use_stars_for_node_size']
+                                        : Config::$config['use_stars_for_node_size']),
+
+        'vary_node_colors' => (isset($requestVars['vary_node_colors'])
+                              && $requestVars['vary_node_colors'] !== ''
+                                ? $requestVars['vary_node_colors']
+                                : Config::$config['vary_node_colors']),
+
+    );
+
+    # create all the variables in the current scope
+    extract($tree_var_names_w_default_values);
+
+    # easy access to var names for other things that need it (e.g. vars_form)
+    $tree_var_names = array_keys($tree_var_names_w_default_values);
+
+    # some of the vars affect the Config vars
+    Config::$config['use_stars_for_node_size'] = $use_stars_for_node_size;
+    Config::$config['vary_node_colors'] = $vary_node_colors;
+
+    # make sure all details are set for all relationships (default to null)
     foreach ($parent_relationships as $no => $relationship) {
         if (!isset($parent_relationships[$no]['child_table']))
             $parent_relationships[$no]['child_table'] = null;
@@ -60,52 +87,24 @@
             $parent_relationships[$no]['parent_filter_field'] = null;
         if (!isset($parent_relationships[$no]['parent_filter_field_val']))
             $parent_relationships[$no]['parent_filter_field_val'] = null;
-
-        /*
-        if (!isset($relationship['child_table'])
-            || !$relationship['child_table']
-        ) {
-            $parent_relationships[$no]['child_table'] = $root_table;
-        }
-
-        if (!isset($relationship['parent_table'])
-            || !$relationship['parent_table']
-        ) {
-            $parent_relationships[$no]['parent_table'] = $root_table;
-        }
-        */
-
     }
 
-    # mostly color for now
-    $table_info = array();
-    # use keys for uniqueness
-    $table_info[$root_table] = array(
-        'color' => name_to_rgb($root_table)
-    );
-    foreach ($parent_relationships as $relationship) {
-        $table = $relationship['parent_table'];
-        $table_info[$table] = array(
-            'color' => name_to_rgb($table)
+    { # node color stuff
+        $table_info = array();
+        # use keys for uniqueness
+        $table_info[$root_table] = array(
+            'color' => name_to_rgb($root_table)
         );
+        foreach ($parent_relationships as $relationship) {
+            $table = $relationship['parent_table'];
+            $table_info[$table] = array(
+                'color' => name_to_rgb($table)
+            );
 
-        $table = $relationship['child_table'];
-        $table_info[$table] = array(
-            'color' => name_to_rgb($table)
-        );
+            $table = $relationship['child_table'];
+            $table_info[$table] = array(
+                'color' => name_to_rgb($table)
+            );
+        }
     }
-
-    Config::$config['use_stars_for_node_size']
-    = $use_stars_for_node_size
-        = (isset($requestVars['use_stars_for_node_size'])
-          && $requestVars['use_stars_for_node_size'] !== ''
-            ? $requestVars['use_stars_for_node_size']
-            : Config::$config['use_stars_for_node_size']);
-
-    Config::$config['vary_node_colors']
-    = $vary_node_colors
-        = (isset($requestVars['vary_node_colors'])
-          && $requestVars['vary_node_colors'] !== ''
-            ? $requestVars['vary_node_colors']
-            : Config::$config['vary_node_colors']);
 
