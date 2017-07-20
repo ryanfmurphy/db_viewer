db1631e7fbc393c99a49212e0303f6476b624107
Ryan Murphy <ryanfmurphy@gmail.com>
2017-07-20 16:38:36 -0700

    fix incorrect CSS merge (obj_editor)

--- obj_editor/style.css.php
+++ obj_editor/style.css.php
@@ -6,6 +6,22 @@ body {
     font-family: sans-serif;
     <?php #box-sizing: border-box; /* e.g. makes the input and textarea exactly the same width */ ?>
 }
+.header_image {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-height: 250px;
+}
+#main_container {
+    /* center the form */
+    width: 50rem;
+    margin-left: auto;
+    margin-right: auto;
+}
+input, select, textarea {
+    font-family: inherit;
+    font-size: 80%;
+}
 @media (min-width: <?= $mobile_size-1 ?>px) {
     #main_container {
         /* center the form */
