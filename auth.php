aa23338dbef7d65864dcac234f6b620dacc222ae
Ryan Murphy <ryanfmurphy@gmail.com>
2017-08-08 12:52:35 -0700

    make auth page prettier

--- auth.php
+++ auth.php
@@ -4,17 +4,55 @@
     require("$trunk/includes/init.php");
 
     $uri = $table_view_uri;
+    $app_name = 'DB Viewer';
 ?>
 <html>
     <head>
+        <style>
+            body {
+                font-family: sans-serif;
+                width: 30rem;
+                margin: 5rem auto;
+                font-size: 100%;
+<?php
+    if ($background == 'dark') {
+?>
+                background: black;
+                color: white;
+<?php
+    }
+?>
+            }
+            h1 {
+                text-align: center;
+            }
+            form div.form_row {
+                margin: .5em;
+                text-align: center;
+            }
+            form input {
+                width: 15rem;
+                font-size: 150%;
+            }
+            form input[type=submit] {
+                width: initial;
+                margin-top: 1rem;
+            }
+        </style>
     </head>
 
     <h1>
-        LOGIN
+        <?= $app_name ?> - Login
     </h1>
     <form action="<?= $uri ?>" method="post">
-        <input name="db_user">
-        <input name="db_password" type="password">
-        <input type="submit" value="submit">
+        <div class="form_row">
+            <input name="db_user" placeholder="Username">
+        </div>
+        <div class="form_row">
+            <input name="db_password" type="password" placeholder="Password">
+        </div>
+        <div class="form_row">
+            <input type="submit" value="submit">
+        </div>
     </form>
 </html>
