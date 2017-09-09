b206155551af98706cb7c1624395c8debec88d06
ryanfmurphy <ryanfmurphy@gmail.com>
2017-09-09 01:26:11 -0500

    table_view: fix broken call to echo_tree_button

--- table_view/html/results_table.php
+++ table_view/html/results_table.php
@@ -226,7 +226,7 @@
 
                                     # delete
                                     if ($include_row_tree_button) {
-                                        TableView::echo_tree_button(
+                                        TreeView::echo_tree_button(
                                             $obj_editor_uri, $tablename_no_quotes, $primary_key
                                         );
                                     }
