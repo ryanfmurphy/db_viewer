e0ba383dc518a237ad19d73c6fce23a68a77f3e7
ryanfmurphy <ryanfmurphy@gmail.com>
2017-09-25 11:42:29 -0500

    SQL schema update

--- sql/migrations/2017-09-24__tree_relationship__drop_single_table_fields.sql
+++ sql/migrations/2017-09-24__tree_relationship__drop_single_table_fields.sql
@@ -1,3 +1,9 @@
 -- 2017-09-24
 alter table tree_relationship drop column child_table;
 alter table tree_relationship drop column parent_table;
+
+-- rm overbearing constraint
+alter table tree_relationship drop constraint tree_relationship_parent_tables_check;
+alter table tree_relationship drop constraint tree_relationship_child_tables_check;
+alter table tree_relationship alter parent_tables drop not null;
+alter table tree_relationship alter child_tables drop not null;
