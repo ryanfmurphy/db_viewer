003671d84b642ff23a627546885c23beea0938b3
ryanfmurphy <ryanfmurphy@gmail.com>
2018-03-01 22:25:21 -0600

    SimpleParser test: block-comments-tricky

---
+++ classes/tests/SimpleParser/003-block-comments-tricky.php
@@ -0,0 +1,21 @@
+<?php
+# run in /classes dir
+include('SimpleParser.php');
+$s = new SimpleParser(['line_comment'=>'//']);
+
+$txt =
+"select \"a\",\"b\",\"c\",'/* my harmless string */' as gotcha_field /* What comments will be removed? */
+    from (select * from table where a='hello') t
+";
+
+echo "\ninitially txt = '$txt'";
+
+$new_txt = $s->blank_out_block_comments($txt);
+echo "\nnow txt = '$new_txt'";
+
+echo "\n";
+echo "It didn't trip over the /* */ in the string because\n";
+echo "first it temporarily blanked out the strings:\n\n";
+
+echo $s->separate_strings($txt)[0]."\n";
+
