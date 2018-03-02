a1d6eb2d22791e5fe93804dfea27346fe6996118
ryanfmurphy <ryanfmurphy@gmail.com>
2018-03-01 23:20:07 -0600

    an example of a SimpleParser test that fails
    I need to be a little more left-to-right
    because otherwise the order-of-operations will get me

---
+++ classes/tests/SimpleParser/004-comments-tricky.php
@@ -0,0 +1,20 @@
+<?php
+include('../../SimpleParser.php');
+$s = new SimpleParser(['line_comment'=>'//']);
+
+$txt =
+"select \"a\",\"b\",\"c\",'/* my harmless string */' as gotcha_field /* What comments will be removed? */
+    from (select * from table where a='hello') t -- here is a subquery
+    where a='hello' -- but hopefully this doesn't cause an issue /*
+    /* this is separate from the line above */
+";
+
+echo "\ninitially txt = [[$txt]]";
+
+echo "\n";
+echo "the seperate_strings() gave:\n";
+print_r($s->separate_strings($txt));
+
+$new_txt = $s->blank_out_comments($txt);
+echo "\nafter blank_block_comments(), txt = [[$new_txt]]";
+
