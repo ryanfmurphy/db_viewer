a8eb69d7d054c7413b8163ccb8b36345b5fb01f8
ryanfmurphy <ryanfmurphy@gmail.com>
2018-05-28 17:34:41 -0500

    Grammar function stubs

--- classes/Grammar.php
+++ classes/Grammar.php
@@ -4,5 +4,24 @@ include('SimpleParser.php');
 
 class Grammar {
 
+    public $production_rules;
+    public $simple_parser;
+
+    function __construct($production_rules, $simple_parser = null) {
+        $this->production_rules = $production_rules;
+        $this->simple_parser = ($simple_parser === null
+                                    ? new SimpleParser()
+                                    : $simple_parser);
+    }
+
+    # e.g. rule_text = '<subject> <predicate>.' or '<item_with_comma>+ and <item>'
+    function parse_rule_text($rule_text) {
+        
+    }
+
+    function parse($input) {
+        
+    }
+
 }
 
