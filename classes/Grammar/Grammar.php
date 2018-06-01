ebcd863407541037c8685794b114c46b7b591d74
ryanfmurphy <ryanfmurphy@gmail.com>
2018-05-31 23:05:51 -0500

    more tests for simple Grammar::parse_rule_text

--- classes/Grammar/Grammar.php
+++ classes/Grammar/Grammar.php
@@ -8,6 +8,9 @@ class Grammar {
     public $production_rules; # should be an array
     public $simple_parser;    # type SimpleParser
 
+    public $var_regex = '/<(\\w+)>/'; # regex for <variables> found in rule_text etc
+                                      # could be terminal or non-terminal
+
     function __construct($terminal_regexes, $production_rules, $simple_parser = null) {
         $this->terminal_regexes = $terminal_regexes; # should be an array
         $this->production_rules = $production_rules; # should be an array
@@ -19,17 +22,15 @@ class Grammar {
     # e.g. rule_text = '<subject> <predicate>.' or '<item_with_comma>+ and <item>'
     function parse_rule_text($rule_text) {
         return preg_replace_callback_offset(
-            '/<(\\w+)>/',
+            $this->var_regex,
             function($match) {
                 $symbol_name = $match[1][0];
                 if (isset($this->terminal_regexes[$symbol_name])) {
                     return '('.$this->terminal_regexes[$symbol_name].')'; # () for clean separation / nesting within regex
                 }
-                elseif (isset($this->production_rules[$symbol_name])) {
-                    return "[RULE:".$this->production_rules[$symbol_name]."]";
-                }
                 else {
-                    return '???';
+                    $full_match_txt = $match[0][0];
+                    return $full_match_txt; # leave unmodified
                 }
             },
             $rule_text
--- classes/Grammar/tests/001-grammar.php
+++ classes/Grammar/tests/001-grammar.php
@@ -3,7 +3,7 @@ include('../Grammar.php');
 $g = new Grammar(
     [
         'word' => '\b\w+\b',        
-        'bird' => '\b(hummingbird|canary|bluebird|pigeon|chicken|turkey|eagle|duck)\b',        
+        'bird' => '\b(hummingbird|canary|bluebird|pigeon|chicken|turkey|eagle|duck|penguin)\b',        
     ],
     [
         'affirmation' => '<word> to the <bird>',
@@ -12,10 +12,35 @@ $g = new Grammar(
 );
 
 $txt = '<word> to the <bird>';
-$result = $g->parse_rule_text($txt);
+$regex = $g->parse_rule_text($txt);
 
 assert(
-    $result == '(\\b\\w+\\b) to the (\\b(hummingbird|canary|bluebird|pigeon|chicken|turkey|eagle|duck)\\b)',
+    $regex == '(\\b\\w+\\b) to the (\\b(hummingbird|canary|bluebird|pigeon|chicken|turkey|eagle|duck|penguin)\\b)',
     'parse_rule_text swaps in terminal regexes'
 ) or die(1);
-echo "PASSED!\n";
+echo "1 PASSED!\n";
+
+assert(
+    preg_match("/^$regex$/", "sudafed to the canary"),
+    'generated regex matches example sentence that conforms to rule'
+) or die(1);
+echo "2 PASSED!\n";
+
+assert(
+    preg_match("/^$regex$/", "umbrellas to the hummingbird"),
+    'generated regex matches example sentence that conforms to rule'
+) or die(1);
+echo "3 PASSED!\n";
+
+assert(
+    !preg_match("/^$regex$/", "martinis with the penguin"),
+    'generated regex doesn\'t match example sentence that doesn\'t conform to rule'
+) or die(1);
+echo "4 PASSED!\n";
+
+assert(
+    !preg_match("/^$regex$/", "props to the aardvark"),
+    'generated regex doesn\'t match example sentence that doesn\'t conform to rule'
+) or die(1);
+echo "5 PASSED!\n";
+
