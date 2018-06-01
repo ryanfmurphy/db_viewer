65703f98976cb4e123d3e5375817a395ba97e2cf
ryanfmurphy <ryanfmurphy@gmail.com>
2018-05-31 22:36:29 -0500

    initial parse_production_rule stub for Grammar

--- classes/Grammar.php
+++ classes/Grammar.php
@@ -4,10 +4,12 @@ include('SimpleParser.php');
 
 class Grammar {
 
+    public $terminal_regexes;
     public $production_rules;
     public $simple_parser;
 
-    function __construct($production_rules, $simple_parser = null) {
+    function __construct($terminal_regexes, $production_rules, $simple_parser = null) {
+        $this->terminal_regexes = $terminal_regexes;
         $this->production_rules = $production_rules;
         $this->simple_parser = ($simple_parser === null
                                     ? new SimpleParser()
@@ -16,7 +18,13 @@ class Grammar {
 
     # e.g. rule_text = '<subject> <predicate>.' or '<item_with_comma>+ and <item>'
     function parse_rule_text($rule_text) {
-        
+        return preg_replace_callback_offset(
+            '/<(\\w+)>/',
+            function($match) {
+                return '*test*';
+            },
+            $rule_text
+        );
     }
 
     function parse($input) {
